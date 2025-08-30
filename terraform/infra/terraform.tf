a0b2e83b728db1c1082edcfcd249491ff015b9e9
akshatapshetty24 <akshatapshetty24@gmail.com>
2025-08-30 07:23:23 +0530

    Update terraform.tf

--- terraform/infra/terraform.tf
+++ terraform/infra/terraform.tf
@@ -2,8 +2,8 @@ terraform {
   backend "s3" {
     bucket         = "ksp-tf-ga-eks-v2-terraform-state-bucket"
     key            = "eks/terraform.tfstate"
-    #region         = "ap-south-1"
-    region = var.aws_region
+    region         = "us-east-1"
+    #region = var.aws_region
     dynamodb_table = "ksp-tf-ga-eks-v2-terraform-locks"
     encrypt        = true
   }
